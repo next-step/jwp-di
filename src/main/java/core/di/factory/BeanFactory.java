5ca0ca82113c5bc77eff440eb9ef818c2c62fa76
조정훈 <jeonghoon1107@gmail.com>
2019-08-22 23:53:03 +0900

    feat : beanFactory 구현

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -1,9 +1,13 @@
 package core.di.factory;
 
 import com.google.common.collect.Maps;
+import core.di.exception.BeanCreationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Set;
 
@@ -24,6 +28,46 @@ public class BeanFactory {
     }
 
     public void initialize() {
-
+        preInstanticateBeans.forEach(this::createBeans);
     }
+
+    private void createBeans(Class<?> preInstanticateBean) {
+        try {
+            initBean(preInstanticateBean);
+        } catch (IllegalAccessException | InstantiationException | InvocationTargetException e) {
+            logger.error("bean creation failed. ", e);
+            throw new BeanCreationException();
+        }
+    }
+
+    private void initBean(Class<?> clazz) throws IllegalAccessException, InstantiationException, InvocationTargetException {
+
+        Constructor<?> injectedConstructor = BeanFactoryUtils.getInjectedConstructor(clazz);
+
+        if (injectedConstructor == null) {
+            beans.put(clazz, clazz.newInstance());
+            return;
+        }
+
+        Class<?>[] parameterTypes = injectedConstructor.getParameterTypes();
+
+        Class<?>[] parameterConcreteClasses = findConcreteClasses(parameterTypes);
+
+        for (Class<?> childClass : parameterConcreteClasses) {
+            initBean(childClass);
+        }
+
+        Object[] classes = Arrays.stream(parameterConcreteClasses)
+                .map(aClass -> beans.get(aClass))
+                .toArray();
+
+        beans.put(clazz, injectedConstructor.newInstance(classes));
+    }
+
+    private Class[] findConcreteClasses(Class<?>[] parameterTypes) {
+        return Arrays.stream(parameterTypes)
+                .map(aClass -> BeanFactoryUtils.findConcreteClass(aClass, preInstanticateBeans))
+                .toArray(Class[]::new);
+    }
+
 }
