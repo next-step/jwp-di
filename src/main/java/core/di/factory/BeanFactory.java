1984dd1534f734b9a39efc8cdb8fac56282d6d01
Summer <jihan805@coupang.com>
2019-08-23 20:29:53 +0900

    feat : implement di

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -4,6 +4,11 @@ import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -23,7 +28,32 @@ public class BeanFactory {
         return (T) beans.get(requiredType);
     }
 
-    public void initialize() {
+    public void initialize() throws IllegalAccessException, InstantiationException, InvocationTargetException {
+        for (Class<?> bean : preInstanticateBeans) {
+            initializeBean(bean);
+        }
+    }
 
+    public void initializeBean(Class<?> bean) throws IllegalAccessException, InstantiationException, InvocationTargetException {
+        Constructor<?> injectedConstructor = BeanFactoryUtils.getInjectedConstructor(bean);
+        if (injectedConstructor == null) {
+            beans.put(bean, bean.newInstance());
+            return;
+        }
+
+        Parameter[] parameters = injectedConstructor.getParameters();
+        List<Class<?>> concreteClassForParameters = new ArrayList<>();
+        for (Parameter parameter : parameters) {
+            Class<?> type = parameter.getType();
+            Class<?> concreteClass = BeanFactoryUtils.findConcreteClass(type, preInstanticateBeans);
+            concreteClassForParameters.add(concreteClass);
+            initializeBean(concreteClass);
+        }
+
+        Object[] objects = concreteClassForParameters.stream()
+                .map(clazz -> beans.get(clazz))
+                .toArray();
+
+        beans.put(bean, injectedConstructor.newInstance(objects));
     }
 }
