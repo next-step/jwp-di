b7f1800e2f7ac47cc5e00a1a8e5aadfe38b0cd47
윤현준 <homelus@daum.net>
2019-08-25 17:48:24 +0900

    test: add bean scanner test

---
+++ src/test/java/core/di/factory/BeanScannerTest.java
@@ -0,0 +1,31 @@
+package core.di.factory;
+
+import core.annotation.Component;
+import core.annotation.web.Controller;
+import core.di.factory.example.QnaController;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BeanScannerTest {
+
+    private BeanScanner beanScanner;
+
+    @BeforeEach
+    void setUp() {
+        beanScanner = new BeanScanner("core.di.factory.example");
+    }
+
+    @DisplayName("Controller 애노테이션의 스캔 여부")
+    @Test
+    @SuppressWarnings("unchecked")
+    void beanScannerWithController() {
+        final Set<Class<?>> classes = beanScanner.scan(Controller.class);
+        assertTrue(classes.contains(QnaController.class));
+    }
+
+}
