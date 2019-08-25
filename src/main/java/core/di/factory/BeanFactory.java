3fcae9b043d1eed0f731f10302b19a4775ce1340
changjun89 <leechang0423@naver.com>
2019-08-26 01:33:38 +0900

    feat #step1 DI 구현

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -3,7 +3,11 @@ package core.di.factory;
 import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -24,6 +28,40 @@ public class BeanFactory {
     }
 
     public void initialize() {
-
+        for (Class<?> clazz : preInstanticateBeans) {
+            initBeans(clazz);
+        }
     }
+
+    private void initBeans(Class<?> clazz) {
+        Constructor<?> injectedConstructor = BeanFactoryUtils.getInjectedConstructor(clazz);
+        beans.put(clazz,getInstance(clazz,injectedConstructor));
+    }
+
+    private Object getInstance(Class<?> clazz, Constructor<?> injectedConstructor) {
+        return injectedConstructor == null ? BeanUtils.instantiateClass(clazz) : getInjectInstance(injectedConstructor);
+    }
+
+    private Object getInjectInstance(Constructor<?> injectedConstructor) {
+        Class<?>[] parameterTypes = injectedConstructor.getParameterTypes();
+        List params = makeConstructorParams(parameterTypes);
+        return BeanUtils.instantiateClass(injectedConstructor, params.stream().toArray());
+    }
+
+    private List makeConstructorParams(Class<?>[] parameterTypes) {
+        List params = new ArrayList();
+        for (Class clazz : parameterTypes) {
+            params.add(getConcreteClass(clazz));
+        }
+        return params;
+    }
+
+    private Object getConcreteClass(Class clazz) {
+        Class<?> concreteClass = BeanFactoryUtils.findConcreteClass(clazz, preInstanticateBeans);
+        if(beans.get(concreteClass) == null) {
+            initBeans(concreteClass);
+        }
+        return beans.get(concreteClass);
+    }
+
 }
