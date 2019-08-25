5ef3b09293d6c22d94719aa4caf344712295a70e
Hwatu(박현식) <phs1116@gmail.com>
2019-08-25 20:46:25 +0900

    feat BeanFactory 구현

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -3,9 +3,11 @@ package core.di.factory;
 import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 
-import java.util.Map;
-import java.util.Set;
+import java.lang.reflect.Constructor;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class BeanFactory {
     private static final Logger logger = LoggerFactory.getLogger(BeanFactory.class);
@@ -24,6 +26,39 @@ public class BeanFactory {
     }
 
     public void initialize() {
+        for (Class<?> preInstanticateBean : preInstanticateBeans) {
+            Object bean = instantiate(preInstanticateBean);
+            beans.put(preInstanticateBean, bean);
+        }
+    }
 
+    private Object instantiate(Class<?> beanClass) {
+        Constructor<?> injectedConstructor = BeanFactoryUtils.getInjectedConstructor(beanClass);
+
+        if (Objects.isNull(injectedConstructor)) {
+            return BeanUtils.instantiateClass(beanClass);
+        }
+
+        Class<?>[] constructorParameterClasses = injectedConstructor.getParameterTypes();
+        Object[] args = getConstructorParameters(constructorParameterClasses);
+
+        return BeanUtils.instantiateClass(injectedConstructor, args);
+    }
+
+    private Object[] getConstructorParameters(Class<?>[] constructorParameterClasses) {
+        return Arrays.stream(constructorParameterClasses)
+                .map(this::createOrGetBean)
+                .collect(Collectors.toList())
+                .toArray();
+    }
+
+    private Object createOrGetBean(Class<?> constructorParameterClass) {
+        Class<?> concreteClass = BeanFactoryUtils.findConcreteClass(constructorParameterClass, preInstanticateBeans);
+        Object bean = beans.get(concreteClass);
+        if (Objects.isNull(bean)) {
+            bean = instantiate(concreteClass);
+            beans.put(concreteClass, bean);
+        }
+        return bean;
     }
 }
