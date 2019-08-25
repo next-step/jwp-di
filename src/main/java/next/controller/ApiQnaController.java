604ffc10f727ec68273f8d0d28e61129c6ea1a31
Jaeyeonling <jaeyeonling@gmail.com>
2019-08-25 09:48:43 +0900

    [refactor] 기존 서비스 Bean으로 변경

--- src/main/java/next/controller/ApiQnaController.java
+++ src/main/java/next/controller/ApiQnaController.java
@@ -1,5 +1,6 @@
 package next.controller;
 
+import core.annotation.Inject;
 import core.annotation.web.Controller;
 import core.annotation.web.RequestMapping;
 import core.annotation.web.RequestMethod;
@@ -19,10 +20,18 @@ import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class ApiQnaController extends AbstractNewController {
-    private static final Logger logger = LoggerFactory.getLogger( ApiQnaController.class );
 
-    private QuestionDao questionDao = QuestionDao.getInstance();
-    private AnswerDao answerDao = AnswerDao.getInstance();
+    private static final Logger logger = LoggerFactory.getLogger(ApiQnaController.class);
+
+    private final QuestionDao questionDao;
+    private final AnswerDao answerDao;
+
+    @Inject
+    public ApiQnaController(final QuestionDao questionDao,
+                            final AnswerDao answerDao) {
+        this.questionDao = questionDao;
+        this.answerDao = answerDao;
+    }
 
     @RequestMapping(value = "/api/qna/list", method = RequestMethod.GET)
     public ModelAndView questions(HttpServletRequest req, HttpServletResponse resp) throws Exception {
