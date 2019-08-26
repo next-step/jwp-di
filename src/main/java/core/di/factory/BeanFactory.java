294d9016810facc61cfa638f709012d5754a1a79
internet200q <internet200@qdillion.com>
2019-08-26 10:17:04 +0900

    DI 구현

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -1,9 +1,18 @@
 package core.di.factory;
 
 import com.google.common.collect.Maps;
+import core.annotation.Inject;
+import core.annotation.web.Controller;
+import core.annotation.web.RequestMapping;
+import core.mvc.tobe.HandlerExecution;
+import core.mvc.tobe.HandlerKey;
+import org.reflections.Reflections;
+import org.reflections.scanners.MethodAnnotationsScanner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import java.util.Map;
 import java.util.Set;
 
@@ -24,6 +33,44 @@ public class BeanFactory {
     }
 
     public void initialize() {
-
+        preInstanticateBeans.stream()
+                .forEach(clazz -> beans.put(clazz, findAndInitInject(clazz)));
     }
+
+    private Object findAndInitInject(Class<?> clazz){
+        try {
+            clazz = BeanFactoryUtils.findConcreteClass(clazz, preInstanticateBeans);
+            return getNewInstance(clazz, BeanFactoryUtils.getInjectedConstructor(clazz));
+        }catch (Exception e){
+            logger.error("injection Error {}", e.getMessage());
+        }
+
+        return null;
+    }
+
+    private Object getNewInstance(Class<?> clazz, Constructor constructor) throws Exception{
+        if(constructor == null){
+            return clazz.newInstance();
+        }
+
+        Object[] parameterObject = constructor.getParameterTypes();
+        Object[] paramList = new Object[parameterObject.length];
+
+        for (int i = 0; i < parameterObject.length; i++) {
+            paramList[i] = findAndInitInject((Class<?>) parameterObject[i]);
+        }
+
+        return constructor.newInstance(paramList);
+    }
+
+    public Map<Class<?>, Object> getControllers() {
+        Map<Class<?>, Object> controllers = Maps.newHashMap();
+        for (Class<?> clazz : preInstanticateBeans) {
+            if (clazz.isAnnotationPresent(Controller.class)) {
+                controllers.put(clazz, beans.get(clazz));
+            }
+        }
+        return controllers;
+    }
+
 }
