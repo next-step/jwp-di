edb7810433fac612c79dbf090be0aeba4d8cc4e0
pporotoss <pporotoss@gmail.com>
2019-08-28 22:13:16 +0900

    feature : BeanFactory 구현

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -3,8 +3,11 @@ package core.di.factory;
 import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 
+import java.lang.reflect.Constructor;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 public class BeanFactory {
@@ -24,6 +27,42 @@ public class BeanFactory {
     }
 
     public void initialize() {
-
+        for (Class<?> beanClass : preInstanticateBeans) {
+            instantiate(beanClass);
+        }
     }
+
+    private Object instantiate(Class<?> beanClass) {
+        Object instance = newInstance(beanClass);
+        this.beans.put(beanClass, instance);
+        return instance;
+    }
+
+    private Object newInstance(Class<?> beanClass) {
+        Class<?> concreteClass = BeanFactoryUtils.findConcreteClass(beanClass, preInstanticateBeans);
+        Constructor<?> constructor = BeanFactoryUtils.getInjectedConstructor(concreteClass);
+        if (Objects.isNull(constructor)) {
+            return BeanUtils.instantiateClass(concreteClass);
+        }
+
+        return BeanUtils.instantiateClass(constructor, getParameterInstances(constructor));
+    }
+
+    private Object[] getParameterInstances(Constructor<?> constructor) {
+        Class<?>[] parameterClasses = constructor.getParameterTypes();
+        Object[] parameters = new Object[parameterClasses.length];
+        for (int i = 0; i < parameterClasses.length; i++) {
+            parameters[i] = getParameterInstance(parameterClasses[i]);
+        }
+        return parameters;
+    }
+
+    private Object getParameterInstance(Class<?> parameterClass) {
+        Object bean = getBean(parameterClass);
+        if (Objects.nonNull(bean)) {
+            return bean;
+        }
+        return instantiate(parameterClass);
+    }
+
 }
