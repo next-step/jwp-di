4a834bdd23c1c9c5da2eef30274c49f3ef4cd7dc
korkorna <korkorna@gmail.com>
2019-08-31 14:56:01 +0900

    add : MyConfiguration

---
+++ src/main/java/next/MyConfiguration.java
@@ -0,0 +1,27 @@
+package next;
+
+import core.annotation.Bean;
+import core.annotation.ComponentScan;
+import core.annotation.Configuration;
+import core.jdbc.JdbcTemplate;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan(basePackages = {"core", "next"})
+public class MyConfiguration {
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource ds = new BasicDataSource();
+        ds.setDriverClassName("org.h2.Driver");
+        ds.setUrl("jdbc:h2:~/jwp-framework;MVCC=TRUE;DB_CLOSE_ON_EXIT=FALSE");
+        ds.setUsername("sa");
+        ds.setPassword("");
+        return ds;
+    }
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+}
