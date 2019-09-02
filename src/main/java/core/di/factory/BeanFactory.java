89a8e0c84b9d88db3041285b3139beb3866762a4
willing1026 <34908960+willing1026@users.noreply.github.com>
2019-09-02 22:58:42 +0900

    # add : di기능구현

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -1,11 +1,17 @@
 package core.di.factory;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 
+import java.lang.reflect.Constructor;
+import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
+import java.util.stream.Stream;
 
 public class BeanFactory {
     private static final Logger logger = LoggerFactory.getLogger(BeanFactory.class);
@@ -24,6 +30,46 @@ public class BeanFactory {
     }
 
     public void initialize() {
+        for (Class instanceType : preInstanticateBeans) {
+            instantiateClass(instanceType);
+        }
+    }
 
+    private Object instantiateClass(Class<?> instanceType) {
+        Class<?> beanType = BeanFactoryUtils.findConcreteClass(instanceType, preInstanticateBeans);
+
+        if (beans.containsKey(beanType)) {
+            return getBean(beanType);
+        }
+
+        Constructor<?> constructor = BeanFactoryUtils.getInjectedConstructor(beanType);
+
+        try {
+            if (constructor == null) {
+                beans.put(beanType, beanType.newInstance());
+                return getBean(beanType);
+            }
+
+            Object bean = instantiateConstructor(constructor);
+            beans.put(beanType, bean);
+            return getBean(beanType);
+        } catch (InstantiationException | IllegalAccessException e) {
+            logger.error(e.toString());
+        }
+
+        throw new NoSuchElementException("wrong Type " + beanType.toString());
+    }
+
+    private Object instantiateConstructor(Constructor<?> constructor) {
+        Class<?>[] parameterTypes = constructor.getParameterTypes();
+        List<Object> parameters = Lists.newArrayList();
+
+        Stream.of(parameterTypes)
+                .forEach(parameterType -> {
+                    Object parameter = instantiateClass(parameterType);
+                    parameters.add(parameter);
+                });
+
+        return BeanUtils.instantiateClass(constructor, parameters.toArray());
     }
 }
