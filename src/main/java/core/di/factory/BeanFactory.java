f40c5ac113289e709091b1bdbad03ecd2a043cfc
Hwang Jaeseon <willing1026@gmail.com>
2019-09-03 23:13:41 +0900

    1단계 - DI 구현 (#27)
    * # add : di기능구현
    
    * # add & modify : bean scanner 추가, annotationHandlerMapping에 적용
    
    ## add
    - BeanScanner : annotation 등록된 클래스 scan (ControllerScanner 제거)
    
    ## modify
    - BeanFactory : 등록된 controller들 정보 반환 메서드 추가
    - AnnotationHandlerMapping : beanSacnner, beanFactory 이용해서 mapping 구현

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -1,21 +1,28 @@
 package core.di.factory;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import core.annotation.web.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 
+import java.lang.reflect.Constructor;
+import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
+import java.util.stream.Stream;
 
 public class BeanFactory {
     private static final Logger logger = LoggerFactory.getLogger(BeanFactory.class);
 
-    private Set<Class<?>> preInstanticateBeans;
+    private Set<Class<?>> preInstantiateBeans;
 
     private Map<Class<?>, Object> beans = Maps.newHashMap();
 
-    public BeanFactory(Set<Class<?>> preInstanticateBeans) {
-        this.preInstanticateBeans = preInstanticateBeans;
+    public BeanFactory(Set<Class<?>> preInstantiateBeans) {
+        this.preInstantiateBeans = preInstantiateBeans;
     }
 
     @SuppressWarnings("unchecked")
@@ -24,6 +31,57 @@ public class BeanFactory {
     }
 
     public void initialize() {
+        for (Class instanceType : preInstantiateBeans) {
+            instantiateClass(instanceType);
+        }
+    }
 
+    public Map<Class<?>, Object> getControllers() {
+        Map<Class<?>, Object> controllers = Maps.newHashMap();
+        preInstantiateBeans.forEach(type -> {
+            if (type.isAnnotationPresent(Controller.class)) {
+                controllers.put(type, getBean(type));
+            }
+        });
+
+        return controllers;
+    }
+
+    private Object instantiateClass(Class<?> instanceType) {
+        Class<?> beanType = BeanFactoryUtils.findConcreteClass(instanceType, preInstantiateBeans);
+
+        if (beans.containsKey(beanType)) {
+            return getBean(beanType);
+        }
+
+        Constructor<?> constructor = BeanFactoryUtils.getInjectedConstructor(beanType);
+
+        try {
+            if (constructor == null) {
+                beans.put(beanType, beanType.newInstance());
+                return getBean(beanType);
+            }
+
+            Object bean = instantiateConstructor(constructor);
+            beans.put(beanType, bean);
+            return getBean(beanType);
+        } catch (InstantiationException | IllegalAccessException e) {
+            logger.error(e.toString());
+        }
+
+        throw new NoSuchElementException("wrong Type " + beanType.toString());
+    }
+
+    private Object instantiateConstructor(Constructor<?> constructor) {
+        Class<?>[] parameterTypes = constructor.getParameterTypes();
+        List<Object> parameters = Lists.newArrayList();
+
+        Stream.of(parameterTypes)
+                .forEach(parameterType -> {
+                    Object parameter = instantiateClass(parameterType);
+                    parameters.add(parameter);
+                });
+
+        return BeanUtils.instantiateClass(constructor, parameters.toArray());
     }
 }
