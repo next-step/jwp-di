a4085ac782187c10ad1e625e36dff1af8faa405e
Juyoung <juy5790@outlook.com>
2019-09-19 21:53:35 +0900

    style step4 webAppInitializer 내부로 패키지 이동

--- src/main/java/core/web/WebApplicationInitializer.java
+++ src/main/java/core/web/WebApplicationInitializer.java
@@ -4,5 +4,6 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
 public interface WebApplicationInitializer {
+
     void onStartup(ServletContext servletContext) throws ServletException;
 }
