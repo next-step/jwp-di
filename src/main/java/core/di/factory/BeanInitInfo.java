7a416f934c08b694f402e109160b3955ee31bef1
nokchax <nokchax@gmail.com>
2020-06-27 22:23:45 +0900

    test: BeanInitInfo equals test

--- src/main/java/core/di/factory/BeanInitInfo.java
+++ src/main/java/core/di/factory/BeanInitInfo.java
@@ -22,6 +22,10 @@ public class BeanInitInfo {
         return classType;
     }
 
+    public Method getMethod() {
+        return method;
+    }
+
     public BeanType getBeanType() {
         return beanType;
     }
--- src/test/java/core/di/factory/BeanInitInfoTest.java
+++ src/test/java/core/di/factory/BeanInitInfoTest.java
@@ -1,15 +1,19 @@
 package core.di.factory;
 
+import core.di.factory.example.ExampleConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("빈을 초기화 하기 위해 필요한 정보들을 담는 클래스")
 class BeanInitInfoTest {
 
     @Test
-    @DisplayName("BeanInitInfo 를 초기화 하기 위해서는 Class를 받아와야한다.")
-    void field() {
-
+    @DisplayName("equals 테스트")
+    void equals() {
+        assertThat(new BeanInitInfo(ExampleConfig.class, BeanType.CONFIGURATION))
+                .isEqualTo(new BeanInitInfo(ExampleConfig.class, BeanType.CONFIGURATION));
     }
 
 }
