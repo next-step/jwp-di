a7d9a2d42a1b68fb3878483145f832f0e04746d2
nokchax <nokchax@gmail.com>
2020-06-28 00:19:33 +0900

    test: ConstructorTypeGenerator's support test

--- src/test/java/core/di/factory/generator/ConstructorTypeGeneratorTest.java
+++ src/test/java/core/di/factory/generator/ConstructorTypeGeneratorTest.java
@@ -2,16 +2,22 @@ package core.di.factory.generator;
 
 import core.di.factory.BeanFactory;
 import core.di.factory.BeanInitInfo;
+import core.di.factory.BeanInitInfoExtractUtil;
 import core.di.factory.BeanType;
-import core.di.factory.example.JdbcQuestionRepository;
-import core.di.factory.example.JdbcUserRepository;
-import core.di.factory.example.MyQnaService;
-import core.di.factory.example.QnaController;
+import core.di.factory.circular.OneComponent;
+import core.di.factory.example.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.*;
+import javax.sql.DataSource;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -33,6 +39,33 @@ class ConstructorTypeGeneratorTest {
                 )
         );
     }
+
+    @ParameterizedTest
+    @MethodSource
+    @DisplayName("제너레이터가 특정 빈타입의 빈 생성을 지원하는지")
+    void support(Class<?> clazz) {
+        Map<Class<?>, BeanInitInfo> beanInitInfos = BeanInitInfoExtractUtil.extractBeanInitInfo(clazz);
+        BeanInitInfo beanInitInfo = beanInitInfos.get(clazz);
+
+        assertThat(generator.support(beanInitInfo)).isTrue();
+    }
+
+    private static Stream<Class<?>> support() {
+        return Stream.of(QnaController.class, MyQnaService.class, JdbcUserRepository.class, OneComponent.class);
+    }
+
+    @Test
+    @DisplayName("@Configuration 클래스는 지원하고, @Bean 은 지원 안하고")
+    void supportConfigurationButBeanNot() {
+        Map<Class<?>, BeanInitInfo> beanInitInfos = BeanInitInfoExtractUtil.extractBeanInitInfo(ExampleConfig.class);
+
+        BeanInitInfo configurationClass = beanInitInfos.get(ExampleConfig.class);
+        assertThat(generator.support(configurationClass)).isTrue();
+
+        BeanInitInfo beanMethod = beanInitInfos.get(DataSource.class);
+        assertThat(generator.support(beanMethod)).isFalse();
+    }
+
 
     @Test
     @DisplayName("추가적인 빈이 필요없는 클래스 생성 테스트")
