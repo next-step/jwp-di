20a18259697e55f61b686f5d17f3252be61a07ee
1000141 <ninjasul@onestorecorp.com>
2020-06-28 08:21:07 +0900

    feat: BeanGetter를 interface로 분리

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -1,21 +1,19 @@
 package core.di.factory;
 
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.ArrayUtils;
-import org.springframework.beans.BeanUtils;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Parameter;
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @Slf4j
 public class BeanFactory {
     private Set<Class<?>> preInstanticateBeans;
 
     private Map<Class<?>, Object> beans = Maps.newHashMap();
+    private List<BeanGetter> beanGetters;
 
     public BeanFactory(Set<Class<?>> preInstanticateBeans) {
         this.preInstanticateBeans = preInstanticateBeans;
@@ -23,6 +21,8 @@ public class BeanFactory {
         preInstanticateBeans.forEach(
             clazz -> log.debug("preInstanticateBeans: {}", clazz.getSimpleName())
         );
+
+        this.beanGetters = Arrays.asList(new ConstructorBeanGetter(preInstanticateBeans, beans));
     }
 
     @SuppressWarnings("unchecked")
@@ -32,61 +32,14 @@ public class BeanFactory {
 
     public void initialize() {
         for (Class<?> beanClass : preInstanticateBeans) {
-            beans.put(beanClass, getBeanInstance(beanClass));
-        }
-        beans.keySet().forEach(clazz -> log.debug("beanClassName: {}", clazz.getSimpleName()));
-    }
-
-    private Object getBeanInstance(Class<?> beanClass) {
-        Constructor constructor = BeanFactoryUtils.getInjectedConstructors(beanClass);
-
-        try {
-            if (hasNoArgument(constructor)) {
-                return getNoArgBeanInstance(beanClass, constructor);
-            }
-
-            return getArgBeanInstance(constructor);
-        }
-        catch (Exception e) {
-            log.error(e.getMessage());
-            return null;
-        }
-    }
-
-    private boolean hasNoArgument(Constructor constructor) {
-        return Objects.isNull(constructor) || ArrayUtils.isEmpty(constructor.getParameters());
-    }
-
-    private Object getNoArgBeanInstance(Class<?> beanClass, Constructor constructor) {
-        if (beans.containsKey(beanClass)) {
-            return beans.get(beanClass);
-        }
-
-        if (Objects.isNull(constructor)) {
-            return BeanUtils.instantiateClass(beanClass);
-        }
-
-        return BeanUtils.instantiateClass(constructor);
-    }
-
-    private Object getArgBeanInstance(Constructor constructor) {
-        Parameter[] parameters = constructor.getParameters();
-        List<Object> arguments = Lists.newArrayList();
-
-        for (Parameter parameter : parameters) {
-            Class<?> concreteClass = BeanFactoryUtils.findConcreteClass(parameter.getType(), preInstanticateBeans);
-
-            if (beans.containsKey(concreteClass)) {
-                arguments.add(beans.get(concreteClass));
-            }
-            else {
-                Object beanInstance = getBeanInstance(concreteClass);
-                beans.put(concreteClass, beanInstance);
-                arguments.add(beanInstance);
+            for (BeanGetter beanGetter : beanGetters) {
+                beans.put(beanClass, beanGetter.getBean(beanClass));
             }
         }
 
-        return BeanUtils.instantiateClass(constructor, arguments.toArray(new Object[0]));
+        beans.keySet().forEach(
+            clazz -> log.debug("beanClassName: {}", clazz.getSimpleName())
+        );
     }
 }
 
---
+++ src/main/java/core/di/factory/BeanGetter.java
@@ -0,0 +1,5 @@
+package core.di.factory;
+
+public interface BeanGetter {
+    Object getBean(Class<?> beanClass);
+}
