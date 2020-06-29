dd4364dacbd1933c2b63ba7e47b4c6f535f89d13
hyeyoom <neoul_chw@icloud.com>
2020-06-30 01:28:51 +0900

    refactor: BeanFactory 만들다보니 interface를 고려 안했음... 흑흑

--- src/main/java/core/di/factory/BeanDefinitionUtil.java
+++ src/main/java/core/di/factory/BeanDefinitionUtil.java
@@ -2,6 +2,7 @@ package core.di.factory;
 
 import com.google.common.collect.Maps;
 import core.annotation.Inject;
+import org.springframework.lang.Nullable;
 
 import java.lang.reflect.Constructor;
 import java.util.Collection;
@@ -10,6 +11,7 @@ import java.util.Optional;
 
 /**
  * TODO: 써보다가 마음에 안들면 책임을 DefaultBeanDefinition으로 이동하자.
+ *
  * @author hyeyoom
  */
 public final class BeanDefinitionUtil {
@@ -17,7 +19,11 @@ public final class BeanDefinitionUtil {
     public static Map<Class<?>, BeanDefinition> convertClassToDefinition(Collection<Class<?>> classes) {
         final Map<Class<?>, BeanDefinition> definitionMap = Maps.newHashMap();
         for (Class<?> clazz : classes) {
-            definitionMap.put(clazz, buildBeanDefinition(clazz));
+            final BeanDefinition beanDefinition = buildBeanDefinition(clazz);
+            definitionMap.put(clazz, beanDefinition);
+            for (Class<?> type : clazz.getInterfaces()) {
+                definitionMap.put(type, beanDefinition);
+            }
         }
         return definitionMap;
     }
@@ -33,13 +39,19 @@ public final class BeanDefinitionUtil {
         return beanDefinition;
     }
 
+    @Nullable
     private static Constructor<?> findBeanConstructor(Class<?> clazz) {
         final Constructor<?>[] ctors = clazz.getDeclaredConstructors();
+        Constructor<?> nonArgsCtor = null;
         for (Constructor<?> ctor : ctors) {
             if (ctor.isAnnotationPresent(Inject.class)) {
                 return ctor;
             }
+
+            if (ctor.getParameterTypes().length == 0) {
+                nonArgsCtor = ctor;
+            }
         }
-        return null;
+        return nonArgsCtor;
     }
 }
