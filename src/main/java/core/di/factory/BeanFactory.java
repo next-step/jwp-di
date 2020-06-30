fd286ad441c56c63e7d29c27069efdd1d4a4c769
bactoria <bactoria@gmail.com>
2020-06-30 16:13:51 +0900

    feat : BeanFactory 구현

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -3,7 +3,10 @@ package core.di.factory;
 import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Set;
 
@@ -24,6 +27,53 @@ public class BeanFactory {
     }
 
     public void initialize() {
-
+        for (Class<?> preInstanticateBean : preInstanticateBeans) {
+            process(preInstanticateBean);
+        }
     }
+
+    private void process(Class<?> preInstanticateBean) {
+        if (beans.containsKey(preInstanticateBean)) {
+            return;
+        }
+
+        final Object instance = makeInstance(preInstanticateBean);
+
+        beans.put(preInstanticateBean, instance);
+    }
+
+    private <T> T makeInstance(Class<T> preInstanticateBean) {
+        final Constructor<T> injectedConstructor = getInjectedConstructor(preInstanticateBean);
+        final Object[] parameters = getParameters(injectedConstructor);
+
+        return BeanUtils.instantiateClass(injectedConstructor, parameters);
+    }
+
+    private <T> Constructor<T> getInjectedConstructor(Class<T> preInstanticateBean) {
+        Constructor<T> constructor = (Constructor<T>) BeanFactoryUtils.getInjectedConstructor(preInstanticateBean);
+        if (constructor == null) {
+            try {
+                constructor = preInstanticateBean.getConstructor();
+            } catch (NoSuchMethodException e) {
+                throw new RuntimeException("인스턴스 생성 실패 : 생성자를 찾을 수 없음.");
+            }
+        }
+        return constructor;
+    }
+
+    private Object[] getParameters(Constructor<?> constructor) {
+        final Class<?>[] parameterTypes = constructor.getParameterTypes();
+        return Arrays.stream(parameterTypes)
+                .map(t -> BeanFactoryUtils.findConcreteClass(t, preInstanticateBeans))
+                .map(this::resolveInstance)
+                .toArray();
+    }
+
+    private Object resolveInstance(Class<?> parameterType) {
+        if (!beans.containsKey(parameterType)) {
+            process(parameterType);
+        }
+        return beans.get(parameterType);
+    }
+
 }
