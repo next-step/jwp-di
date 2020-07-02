0aae3c3ac622c22d17a8220e9af605961d575ff0
hyeyoom <neoul_chw@icloud.com>
2020-07-02 19:08:20 +0900

    refactor: DispatcherServlet이 ContextLoaderListener를 타도록 변경

--- src/main/java/next/support/context/ContextLoaderListener.java
+++ src/main/java/next/support/context/ContextLoaderListener.java
@@ -1,14 +1,14 @@
 package next.support.context;
 
 import core.jdbc.ConnectionManager;
+import core.mvc.DispatcherServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
+import javax.servlet.*;
 import javax.servlet.annotation.WebListener;
 
 @WebListener
@@ -17,11 +17,22 @@ public class ContextLoaderListener implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        initDatabase();
+        initWebApp(sce.getServletContext());
+        logger.info("Completed Load ServletContext!");
+    }
+
+    private void initWebApp(ServletContext servletContext) {
+        final Servlet dispatcherServlet = new DispatcherServlet();
+        final ServletRegistration.Dynamic registration = servletContext.addServlet("dispatcher", dispatcherServlet);
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/");
+    }
+
+    private void initDatabase() {
         ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
         populator.addScript(new ClassPathResource("jwp.sql"));
         DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
-
-        logger.info("Completed Load ServletContext!");
     }
 
     @Override
