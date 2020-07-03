bb07207fc000a4853b1607496b4747d89c23da08
KingCjy <tlssycks@gmail.com>
2020-07-04 04:41:43 +0900

    fix: 코드정리

--- src/main/java/core/di/factory/ClassBeanScanner.java
+++ src/main/java/core/di/factory/ClassBeanScanner.java
@@ -7,24 +7,16 @@ import org.reflections.Reflections;
 import org.reflections.scanners.MethodAnnotationsScanner;
 import org.reflections.scanners.SubTypesScanner;
 import org.reflections.scanners.TypeAnnotationsScanner;
-import org.reflections.util.ClasspathHelper;
-import org.reflections.util.ConfigurationBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.AnnotatedElementUtils;
 
 import java.lang.annotation.Annotation;
-import java.net.URL;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class ClassBeanScanner implements BeanScanner {
 
-    private static final Logger logger = LoggerFactory.getLogger(ClassBeanScanner.class);
+    private static final Class<? extends Annotation>[] SCAN_ANNOTATIONS = new Class[] { Controller.class, Service.class, Repository.class, Component.class, Configuration.class};
 
     private BeanDefinitionRegistry beanDefinitionRegistry;
-
-    private Class<? extends Annotation>[] annotations = new Class[] { Controller.class, Service.class, Repository.class, Component.class, Configuration.class};
 
     public ClassBeanScanner(BeanDefinitionRegistry beanDefinitionRegistry) {
         this.beanDefinitionRegistry = beanDefinitionRegistry;
@@ -38,7 +30,7 @@ public class ClassBeanScanner implements BeanScanner {
     public void scanPackages(Set<Class<?>> scannedClasses, Object... basePackages) {
 
         Reflections reflections = new Reflections(basePackages, new TypeAnnotationsScanner(), new SubTypesScanner(false), new MethodAnnotationsScanner());
-        Set<Class<?>> getAnnotatedClasses = ReflectionUtils.getAnnotatedClasses(reflections, annotations);
+        Set<Class<?>> getAnnotatedClasses = ReflectionUtils.getAnnotatedClasses(reflections, SCAN_ANNOTATIONS);
 
         for (Class<?> targetClass : getAnnotatedClasses) {
             if(scannedClasses.contains(targetClass)) {
