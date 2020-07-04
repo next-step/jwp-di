3cbc0708f0929b2264120cc3e8701a460cd73b70
hyeyoom <neoul_chw@icloud.com>
2020-07-04 18:07:13 +0900

    refactor: 2단계 피드백 - ApplicationContext 사용하도록 수정. DispatcherServlet도 수정. AnnotationHandlerMapping도 수정!

---
+++ src/main/java/next/ApplicationConfiguration.java
@@ -0,0 +1,9 @@
+package next;
+
+import core.annotation.ComponentScan;
+import core.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"core", "next"})
+public class ApplicationConfiguration {
+}
