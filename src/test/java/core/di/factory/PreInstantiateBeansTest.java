d07b717c73ae889bcfbacf8271e37ae7ab823adf
kjm <jm.kang@Qualson.com>
2020-07-16 17:34:25 +0900

    service 인스턴스 생성 테스트 코드 추가

--- src/test/java/core/di/factory/PreInstantiateBeansTest.java
+++ src/test/java/core/di/factory/PreInstantiateBeansTest.java
@@ -1,6 +1,7 @@
 package core.di.factory;
 
 import core.di.factory.example.JdbcUserRepository;
+import core.di.factory.example.MyQnaService;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,10 +9,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PreInstantiateBeansTest {
 
     @Test
-    public void repositoryDi() {
+    public void repositoryCreateBeanObject() {
         PreInstantiateBeans preInstantiateBeans = new PreInstantiateBeans();
 
         Object object = preInstantiateBeans.createBeanObject(JdbcUserRepository.class);
+        assertThat(object).isNotEqualTo(null);
+    }
+
+    @Test
+    public void serviceCreateBeanObject() {
+        PreInstantiateBeans preInstantiateBeans = new PreInstantiateBeans();
+
+        Object object = preInstantiateBeans.createBeanObject(MyQnaService.class);
         assertThat(object).isNotEqualTo(null);
     }
 
