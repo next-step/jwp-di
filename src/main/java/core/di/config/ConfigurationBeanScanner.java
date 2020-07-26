cd1cf9176f8031904ae56e604c908a20e8c8d311
liquidjoo <liquidjoo@gmail.com>
2020-07-27 00:41:31 +0900

    feat : componentScan 활용

--- src/main/java/core/di/config/ConfigurationBeanScanner.java
+++ src/main/java/core/di/config/ConfigurationBeanScanner.java
@@ -1,5 +1,6 @@
 package core.di.config;
 
+import core.annotation.ComponentScan;
 import core.annotation.Configuration;
 import core.di.factory.BeanFactory;
 import org.reflections.Reflections;
@@ -13,7 +14,19 @@ public class ConfigurationBeanScanner {
     public static Map<Class<?>, Object> scan(BeanFactory beanFactory) {
         Reflections reflections = new Reflections("");
         Set<Class<?>> typesAnnotatedWith = reflections.getTypesAnnotatedWith(Configuration.class);
-        beanFactory.applyConfiguration(typesAnnotatedWith);
+        Set<Class<?>> classes = new HashSet<>();
+        typesAnnotatedWith.stream()
+                .filter(clazz -> clazz.isAnnotationPresent(ComponentScan.class))
+                .forEach(clazz -> {
+                    final ComponentScan annotation = clazz.getAnnotation(ComponentScan.class);
+                    final String[] value = annotation.value();
+                    for (final String path : value) {
+                        Reflections componentReflections = new Reflections(path);
+                        classes.addAll(componentReflections.getTypesAnnotatedWith(Configuration.class));
+                    }
+                });
+
+        beanFactory.applyConfiguration(classes);
         return beanFactory.getConfigurationBeans();
     }
 }
--- src/test/java/core/di/config/ConfigurationBeanScannerTest.java
+++ src/test/java/core/di/config/ConfigurationBeanScannerTest.java
@@ -1,14 +1,21 @@
 package core.di.config;
 
+import core.annotation.ComponentScan;
+import core.annotation.Configuration;
 import core.di.factory.BeanFactory;
+import core.di.factory.example.ExampleConfig;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,5 +28,25 @@ class ConfigurationBeanScannerTest {
         final Map<Class<?>, Object> scan = ConfigurationBeanScanner.scan(beanFactory);
         final Object bean = scan.get(DataSource.class);
         assertThat(bean).isNotNull();
+    }
+
+    @Test
+    void getComponentScanClazz() {
+        Reflections reflections = new Reflections("");
+        Set<Class<?>> typesAnnotatedWith = reflections.getTypesAnnotatedWith(Configuration.class);
+        Set<Class<?>> classes = new HashSet<>();
+        typesAnnotatedWith.stream()
+                .filter(clazz -> clazz.isAnnotationPresent(ComponentScan.class))
+                .forEach(clazz -> {
+                    final ComponentScan annotation = clazz.getAnnotation(ComponentScan.class);
+                    final String[] value = annotation.value();
+                    for (final String path : value) {
+                        Reflections reflections1 = new Reflections(path);
+                        classes.addAll(reflections1.getTypesAnnotatedWith(Configuration.class));
+                    }
+                });
+
+        assertThat(classes.contains(MyConfiguration.class)).isTrue();
+        assertThat(classes.contains(ExampleConfig.class)).isTrue();
     }
 }
