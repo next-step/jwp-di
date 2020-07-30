4d80d54ff66da5a97df7de0c8b27e274065515ab
SujungRim <remember2889@gmail.com>
2020-07-30 20:20:22 +0900

    feat(BeanFactory): 빈 생성 및 관계 설정 구현

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -3,7 +3,10 @@ package core.di.factory;
 import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 
+
+import java.lang.reflect.Constructor;
 import java.util.Map;
 import java.util.Set;
 
@@ -24,6 +27,40 @@ public class BeanFactory {
     }
 
     public void initialize() {
-
+        for (Class<?> beanClazz : this.preInstanticateBeans) {
+            Object bean = instantiateBean(beanClazz);
+            beans.put(beanClazz, bean);
+            logger.debug("bean registration complete : {}", beanClazz.getName());
+        }
     }
+
+    private Object instantiateBean(Class<?> beanClazz) {
+        Object bean = getBean(beanClazz);
+        if (bean != null) {
+            return bean;
+        }
+
+        Constructor<?> injectedConstructor = BeanFactoryUtils.getInjectedConstructor(beanClazz);
+        if (injectedConstructor == null) {
+            return BeanUtils.instantiateClass(beanClazz);
+        }
+
+        return instantiateBeanWithArgs(injectedConstructor);
+    }
+
+    private Object instantiateBeanWithArgs(Constructor<?> constructor) {
+        Class<?>[] parameterTypes = constructor.getParameterTypes();
+        Object[] args = new Object[parameterTypes.length];
+
+        for (int i = 0; i < parameterTypes.length; i++) {
+            Class<?> concreteClass = BeanFactoryUtils.findConcreteClass(parameterTypes[i], this.preInstanticateBeans);
+            Object bean = beans.get(concreteClass);
+            if (bean == null) {
+                bean = instantiateBean(concreteClass);
+            }
+            args[i] = bean;
+        }
+        return BeanUtils.instantiateClass(constructor, args);
+    }
+
 }
