7ad8087e7ec514b04901ff58e0c9acde3e9f33d1
SujungRim <remember2889@gmail.com>
2020-08-01 20:32:42 +0900

    fix(BeanScannerTest): 생성자 파라미터 누락 수정

--- src/test/java/core/di/factory/BeanScannerTest.java
+++ src/test/java/core/di/factory/BeanScannerTest.java
@@ -1,5 +1,8 @@
 package core.di.factory;
 
+import core.annotation.Repository;
+import core.annotation.Service;
+import core.annotation.web.Controller;
 import core.di.factory.example.JdbcQuestionRepository;
 import core.di.factory.example.JdbcUserRepository;
 import core.di.factory.example.MyQnaService;
@@ -16,7 +19,7 @@ class BeanScannerTest {
     @Test
     void test_scanBean() {
         // given
-        BeanScanner beanScanner = new BeanScanner();
+        BeanScanner beanScanner = new BeanScanner(Controller.class, Service.class, Repository.class);
         // when
         Set<Class<?>> preInstantiateBeans = beanScanner.scan("core.di.factory.example");
         // then
