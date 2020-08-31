2778b6678cfe77289794ef3fbba3301636307b0b
liquidjoo <liquidjoo@gmail.com>
2020-09-01 01:22:41 +0900

    refactor : 메서드의 책임 분리

--- src/main/java/core/di/config/ConfigurationBeanScanner.java
+++ src/main/java/core/di/config/ConfigurationBeanScanner.java
@@ -5,43 +5,41 @@ import core.annotation.Configuration;
 import core.di.factory.BeanFactory;
 import org.reflections.Reflections;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class ConfigurationBeanScanner {
 
     public static Map<Class<?>, Object> scan(BeanFactory beanFactory) {
-        Reflections reflections = new Reflections("");
-        Set<Class<?>> typesAnnotatedWith = reflections.getTypesAnnotatedWith(Configuration.class);
-        Set<Class<?>> classes = new HashSet<>();
-        typesAnnotatedWith.stream()
-                .filter(clazz -> clazz.isAnnotationPresent(ComponentScan.class))
-                .forEach(clazz -> {
-                    final ComponentScan annotation = clazz.getAnnotation(ComponentScan.class);
-                    final String[] value = annotation.value();
-                    for (final String path : value) {
-                        Reflections componentReflections = new Reflections(path);
-                        classes.addAll(componentReflections.getTypesAnnotatedWith(Configuration.class));
-                    }
-                });
+        beanFactory.applyConfiguration(getClassesByConfigurationType());
+        return beanFactory.getConfigurationBeans();
+    }
 
-        final Set<ComponentScan> componentScans = typesAnnotatedWith.stream()
-                .filter(clazz -> clazz.isAnnotationPresent(ComponentScan.class))
-                .map(clazz -> clazz.getAnnotation(ComponentScan.class))
-                .collect(Collectors.toSet());
-
-        final Set<String> paths = componentScans.stream()
-                .flatMap(s -> Arrays.stream(s.value()))
-                .collect(Collectors.toSet());
-
-        final Set<Class<?>> configClasses = paths.stream()
+    private static Set<Class<?>> getClassesByConfigurationType() {
+        final Set<String> paths = getPathToScan();
+        return paths.stream()
                 .flatMap(path -> {
                     Reflections componentReflections = new Reflections(path);
                     return componentReflections.getTypesAnnotatedWith(Configuration.class).stream();
                 })
                 .collect(Collectors.toSet());
+    }
 
-        beanFactory.applyConfiguration(configClasses);
-        return beanFactory.getConfigurationBeans();
+    private static Set<String> getPathToScan() {
+        final Set<ComponentScan> componentScans = getClassesByComponentScanType();
+        return componentScans.stream()
+                .flatMap(s -> Arrays.stream(s.value()))
+                .collect(Collectors.toSet());
+    }
+
+    private static Set<ComponentScan> getClassesByComponentScanType() {
+        Reflections reflections = new Reflections("");
+        Set<Class<?>> typesAnnotatedWith = reflections.getTypesAnnotatedWith(Configuration.class);
+        return typesAnnotatedWith.stream()
+                .filter(clazz -> clazz.isAnnotationPresent(ComponentScan.class))
+                .map(clazz -> clazz.getAnnotation(ComponentScan.class))
+                .collect(Collectors.toSet());
     }
 }
