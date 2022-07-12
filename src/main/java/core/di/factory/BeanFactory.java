67d3397b157a7ff7c16bd04fe62a106ea8467b96
sjgwon <sjgwon@woowahan.com>
2022-07-12 18:48:17 +0900

    feat: BeanFactory 구현

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -4,7 +4,11 @@ import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 public class BeanFactory {
@@ -24,6 +28,34 @@ public class BeanFactory {
     }
 
     public void initialize() {
+        preInstanticateBeans.forEach(clazz -> beans.put(clazz, beans.computeIfAbsent(clazz, this::getObject)));
+    }
 
+    private Object getObject(Class<?> clazz) {
+        Constructor<?> constructor = getConstructor(clazz);
+        Class<?>[] parameterTypes = constructor.getParameterTypes();
+        Object[] objects = Arrays.stream(parameterTypes)
+                .map(it -> getObject(BeanFactoryUtils.findConcreteClass(it, preInstanticateBeans)))
+                .toArray();
+
+        try {
+            return constructor.newInstance(objects);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Constructor<?> getConstructor(Class<?> clazz) {
+        Constructor<?> injectedConstructor = BeanFactoryUtils.getInjectedConstructor(clazz);
+
+        if (Objects.nonNull(injectedConstructor)) {
+            return injectedConstructor;
+        }
+
+        try {
+            return clazz.getConstructor();
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
