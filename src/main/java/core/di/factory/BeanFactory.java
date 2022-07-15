7f28c69a517c4a15bd3fceb80f330b00bf329a2b
Sungjun <sungjunpizz@gmail.com>
2022-07-15 14:33:10 +0900

    [Step1] DI 구현 (#93)
    * feat: BeanFactory 구현
    
    * feat: production code에 BeanFactory 적용
    
    Co-authored-by: sjgwon <sjgwon@woowahan.com>

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -1,15 +1,17 @@
 package core.di.factory;
 
 import com.google.common.collect.Maps;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import core.annotation.web.Controller;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class BeanFactory {
-    private static final Logger logger = LoggerFactory.getLogger(BeanFactory.class);
-
     private Set<Class<?>> preInstanticateBeans;
 
     private Map<Class<?>, Object> beans = Maps.newHashMap();
@@ -24,6 +26,41 @@ public class BeanFactory {
     }
 
     public void initialize() {
+        preInstanticateBeans.forEach(clazz -> beans.put(clazz, beans.computeIfAbsent(clazz, this::getObject)));
+    }
 
+    public Map<Class<?>, Object> getControllerTypes() {
+        return beans.entrySet()
+                .stream()
+                .filter(it -> it.getKey().isAnnotationPresent(Controller.class))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    private Object getObject(Class<?> clazz) {
+        Constructor<?> constructor = getConstructor(clazz);
+        Class<?>[] parameterTypes = constructor.getParameterTypes();
+        Object[] objects = Arrays.stream(parameterTypes)
+                .map(it -> getObject(BeanFactoryUtils.findConcreteClass(it, preInstanticateBeans)))
+                .toArray();
+
+        try {
+            return constructor.newInstance(objects);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Constructor<?> getConstructor(Class<?> clazz) {
+        Constructor<?> injectedConstructor = BeanFactoryUtils.getInjectedConstructor(clazz);
+
+        if (Objects.nonNull(injectedConstructor)) {
+            return injectedConstructor;
+        }
+
+        try {
+            return clazz.getConstructor();
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/core/di/factory/BeanScanner.java
+++ src/main/java/core/di/factory/BeanScanner.java
@@ -1,10 +1,18 @@
-package core.mvc.tobe;
+package core.di.factory;
 
+import com.google.common.collect.Sets;
+import core.annotation.Repository;
+import core.annotation.Service;
 import core.annotation.web.Controller;
 import core.annotation.web.RequestMapping;
 import core.mvc.tobe.HandlerExecution;
 import core.mvc.tobe.HandlerKey;
-import core.mvc.tobe.support.*;
+import core.mvc.tobe.support.ArgumentResolver;
+import core.mvc.tobe.support.HttpRequestArgumentResolver;
+import core.mvc.tobe.support.HttpResponseArgumentResolver;
+import core.mvc.tobe.support.ModelArgumentResolver;
+import core.mvc.tobe.support.PathVariableArgumentResolver;
+import core.mvc.tobe.support.RequestParamArgumentResolver;
 import org.reflections.Reflections;
 import org.reflections.scanners.MethodAnnotationsScanner;
 import org.reflections.scanners.SubTypesScanner;
@@ -14,23 +22,27 @@ import org.slf4j.LoggerFactory;
 import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
 import org.springframework.core.ParameterNameDiscoverer;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-import static core.util.ReflectionUtils.newInstance;
 import static java.util.Arrays.asList;
 
-public class ControllerScanner {
+public class BeanScanner {
 
-    private static final Logger logger = LoggerFactory.getLogger(core.mvc.tobe.ControllerScanner.class);
+    private static final Logger logger = LoggerFactory.getLogger(BeanScanner.class);
 
     private static final List<ArgumentResolver> argumentResolvers = asList(
-                new HttpRequestArgumentResolver(),
-                new HttpResponseArgumentResolver(),
-                new RequestParamArgumentResolver(),
-                new PathVariableArgumentResolver(),
-                new ModelArgumentResolver()
-        );
+            new HttpRequestArgumentResolver(),
+            new HttpResponseArgumentResolver(),
+            new RequestParamArgumentResolver(),
+            new PathVariableArgumentResolver(),
+            new ModelArgumentResolver()
+    );
 
     private static final ParameterNameDiscoverer nameDiscoverer = new LocalVariableTableParameterNameDiscoverer();
 
@@ -38,11 +50,11 @@ public class ControllerScanner {
         Reflections reflections = new Reflections(basePackage, new TypeAnnotationsScanner(), new SubTypesScanner(), new MethodAnnotationsScanner());
         Map<HandlerKey, HandlerExecution> handlers = new HashMap<>();
 
-        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
-        for (Class<?> controller : controllers) {
-            Object target = newInstance(controller);
-            addHandlerExecution(handlers, target, controller.getMethods());
-        }
+        Set<Class<?>> beans = getTypesAnnotatedWith(reflections, Controller.class, Service.class, Repository.class);
+
+        BeanFactory beanFactory = new BeanFactory(beans);
+        beanFactory.initialize();
+        beanFactory.getControllerTypes().forEach((key, value) -> addHandlerExecution(handlers, value, key.getMethods()));
 
         return handlers;
     }
@@ -59,4 +71,11 @@ public class ControllerScanner {
                 });
     }
 
+    private Set<Class<?>> getTypesAnnotatedWith(Reflections reflections, Class<? extends Annotation>... annotations) {
+        Set<Class<?>> beans = Sets.newHashSet();
+        for (Class<? extends Annotation> annotation : annotations) {
+            beans.addAll(reflections.getTypesAnnotatedWith(annotation));
+        }
+        return beans;
+    }
 }
