6bf3288a27bce79c3e5e897edfc12c0e71d5927e
dongchul <sorkehdcjf@gmail.com>
2022-07-19 22:41:32 +0900

    refactor: Controller Scanner -> Bean Scanner

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -2,10 +2,12 @@ package core.di.factory;
 
 import com.google.common.collect.Maps;
 import core.annotation.Inject;
+import core.annotation.web.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 
+import java.lang.reflect.Constructor;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -35,22 +37,7 @@ public class BeanFactory {
         while (!beans.keySet().containsAll(preInstanticateBeans)) {
             preInstanticateBeans.stream()
                                 .filter(bean -> !beans.containsKey(bean))
-                                .forEach(bean -> {
-                                    Arrays.stream(bean.getDeclaredConstructors())
-                                          .filter(constructor -> !beans.containsKey(bean) && constructor.isAnnotationPresent(Inject.class))
-                                          .forEach(constructor -> {
-                                              Class<?>[] parameterTypes = constructor.getParameterTypes();
-                                              Set<Class<?>> classes = beans.keySet();
-                                              if (classes.containsAll(Set.of(parameterTypes))) {
-                                                  List<Object> objects = Arrays.stream(parameterTypes)
-                                                                               .map(parameterType -> beans.get(parameterType))
-                                                                               .collect(Collectors.toList());
-                                                  Object o = BeanUtils.instantiateClass(constructor, objects.toArray());
-                                                  Arrays.stream(bean.getInterfaces()).forEach(aClass -> beans.put(aClass, o));
-                                                  beans.put(bean, o);
-                                              }
-                                          });
-            });
+                                .forEach(bean -> initDependentBeans(bean));
             if (initializedBeanCount == beans.size()) {
                 throw new IllegalArgumentException("생성 불가능한 빈이 존재합니다.");
             }
@@ -59,14 +46,45 @@ public class BeanFactory {
     }
 
     private void initIndependentBeans() {
-        preInstanticateBeans.forEach(bean -> {
-            Arrays.stream(bean.getDeclaredConstructors())
-                  .filter(method -> !beans.containsKey(bean) && !method.isAnnotationPresent(Inject.class))
-                  .forEach(__ -> {
-                      Object o = BeanUtils.instantiateClass(bean);
-                      Arrays.stream(bean.getInterfaces()).forEach(aClass -> beans.put(aClass, o));
-                      beans.put(bean, o);
-                  });
-        });
+        preInstanticateBeans.forEach(bean -> createIndependentBean(bean));
+    }
+
+    private void createIndependentBean(Class<?> bean) {
+        Arrays.stream(bean.getDeclaredConstructors())
+              .filter(constructor -> !beans.containsKey(bean) && !constructor.isAnnotationPresent(Inject.class))
+              .forEach(__ -> {
+                  Object obj = BeanUtils.instantiateClass(bean);
+                  Arrays.stream(bean.getInterfaces()).forEach(aClass -> beans.put(aClass, obj));
+                  beans.put(bean, obj);
+              });
+    }
+
+    private void initDependentBeans(Class<?> bean) {
+        Arrays.stream(bean.getDeclaredConstructors())
+              .filter(constructor -> !beans.containsKey(bean) && constructor.isAnnotationPresent(Inject.class))
+              .forEach(constructor -> createDependentBean(bean, constructor));
+    }
+
+    private void createDependentBean(Class<?> bean, Constructor<?> constructor) {
+        Class<?>[] parameterTypes = constructor.getParameterTypes();
+        Set<Class<?>> classes = beans.keySet();
+        if (!classes.containsAll(Set.of(parameterTypes))) {
+            return;
+        }
+
+        List<Object> parameters = Arrays.stream(parameterTypes)
+                                     .map(parameterType -> beans.get(parameterType))
+                                     .collect(Collectors.toList());
+        Object obj = BeanUtils.instantiateClass(constructor, parameters.toArray());
+        Arrays.stream(bean.getInterfaces()).forEach(aClass -> beans.put(aClass, obj));
+        beans.put(bean, obj);
+    }
+
+    public Set<Object> getControllers() {
+        return beans.keySet()
+                     .stream()
+                     .filter(beanClass -> beanClass.isAnnotationPresent(Controller.class))
+                     .map(controllerBean -> beans.get(controllerBean))
+                     .collect(Collectors.toSet());
     }
 }
--- src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
+++ src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
@@ -12,19 +12,17 @@ import java.util.Map;
 public class AnnotationHandlerMapping implements HandlerMapping {
     private static final Logger logger = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
-    private Object[] basePackage;
-    private ControllerScanner controllerScanner;
+    private BeanScanner beanScanner;
 
     private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
 
     public AnnotationHandlerMapping(Object... basePackage) {
-        this.basePackage = basePackage;
-        controllerScanner = new ControllerScanner();
+        beanScanner = new BeanScanner(basePackage);
     }
 
     public void initialize() {
         logger.info("## Initialized Annotation Handler Mapping");
-        handlerExecutions.putAll(controllerScanner.scan(basePackage));
+        handlerExecutions.putAll(beanScanner.scanController());
     }
 
     public Object getHandler(HttpServletRequest request) {
