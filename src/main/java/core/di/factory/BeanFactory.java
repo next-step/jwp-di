7019b0acefc0d141e3d97ed96c2e4c2891c4dcfb
chyee <chyee@coupang.com>
2022-08-04 15:54:33 +0900

    feat - implement BeanFactory

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -3,19 +3,23 @@ package core.di.factory;
 import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 public class BeanFactory {
     private static final Logger logger = LoggerFactory.getLogger(BeanFactory.class);
 
-    private Set<Class<?>> preInstanticateBeans;
+    private Set<Class<?>> preInstantiateBeanClazzs;
 
     private Map<Class<?>, Object> beans = Maps.newHashMap();
 
-    public BeanFactory(Set<Class<?>> preInstanticateBeans) {
-        this.preInstanticateBeans = preInstanticateBeans;
+    public BeanFactory(Set<Class<?>> preInstantiateBeanClazzs) {
+        this.preInstantiateBeanClazzs = preInstantiateBeanClazzs;
     }
 
     @SuppressWarnings("unchecked")
@@ -24,6 +28,26 @@ public class BeanFactory {
     }
 
     public void initialize() {
+        for (Class<?> clazz : preInstantiateBeanClazzs) {
+            Object bean = instantiateBean(clazz);
+            beans.put(clazz, bean);
+        }
+    }
 
+    private Object instantiateBean(Class<?> clazz) {
+        Class<?> concreteClass = BeanFactoryUtils.findConcreteClass(clazz, preInstantiateBeanClazzs);
+        Constructor<?> constructor = BeanFactoryUtils.getInjectedConstructor(concreteClass);
+        if (Objects.isNull(constructor)) {
+            return BeanUtils.instantiateClass(concreteClass);
+        }
+
+        try {
+            Object[] params = Arrays.stream(constructor.getParameters())
+                    .map(param -> instantiateBean(param.getType()))
+                    .toArray();
+            return constructor.newInstance(params);
+        } catch (Exception e) {
+            throw new RuntimeException("fail to initiate clazz for bean: " + clazz, e);
+        }
     }
 }
