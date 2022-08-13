a1054c05ec9b0e7e139b7280bed0ba5a9077d4ba
devson <sonyc5720@gmail.com>
2022-08-13 21:00:10 +0900

    feat: initialize 메서드 구현

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -4,6 +4,9 @@ import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Set;
 
@@ -24,6 +27,38 @@ public class BeanFactory {
     }
 
     public void initialize() {
+        for (Class<?> preInstanticateBean : this.preInstanticateBeans) {
+            this.initialize(preInstanticateBean);
+        }
+    }
 
+    private Object initialize(Class<?> preInstanticateBean) {
+        if (this.beans.containsKey(preInstanticateBean)) {
+            return this.beans.get(preInstanticateBean);
+        }
+
+        Class<?> concretePreInstanticateBean = BeanFactoryUtils.findConcreteClass(preInstanticateBean, this.preInstanticateBeans);
+        Class<?>[] injectedClasses = BeanFactoryUtils.getInjectedClasses(concretePreInstanticateBean);
+        Object[] injectedBeans = Arrays.stream(injectedClasses)
+                .map(this::initialize)
+                .toArray();
+
+        Object bean = this.constructBean(concretePreInstanticateBean, injectedBeans);
+        this.beans.put(preInstanticateBean, bean);
+        logger.info("Bean({}) has created", bean);
+        return bean;
+    }
+
+    private Object constructBean(Class<?> preInstanticateBean, Object... beans) {
+        try {
+            return this.getConstructor(preInstanticateBean).newInstance(beans);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Constructor<?> getConstructor(Class<?> preInstanticateBean) throws NoSuchMethodException {
+        Constructor<?> injectedConstructor = BeanFactoryUtils.getInjectedConstructor(preInstanticateBean);
+        return (injectedConstructor != null) ? injectedConstructor : preInstanticateBean.getDeclaredConstructors()[0];
     }
 }
