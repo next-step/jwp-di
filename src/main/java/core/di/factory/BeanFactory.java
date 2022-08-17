caa2bd25efb5f7002d05c900eea494fefc6bca1b
Seondong Pyo <seondongpyo@gmail.com>
2022-08-17 18:49:33 +0900

    [1단계 - DI 구현] 리뷰 요청드립니다. (#101)
    * feat: BeanFactory.initialize() 메서드 구현
    
    * refactor: ControllerScanner 클래스의 이름을 BeanScanner로 변경
    
    * feat: `@Controller` 애노테이션이 붙은 핸들러들을 조회하는 메서드 추가
    
    * refactor: BeanScanner 클래스가 `@Service`, `@Repository` 애노테이션이 붙은 클래스도 스캔하도록 수정

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -1,21 +1,27 @@
 package core.di.factory;
 
-import com.google.common.collect.Maps;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.Constructor;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
+import core.annotation.web.Controller;
 
 public class BeanFactory {
     private static final Logger logger = LoggerFactory.getLogger(BeanFactory.class);
 
-    private Set<Class<?>> preInstanticateBeans;
+    private final Set<Class<?>> preInstantiateBeans;
+    private final Map<Class<?>, Object> beans = Maps.newHashMap();
 
-    private Map<Class<?>, Object> beans = Maps.newHashMap();
-
-    public BeanFactory(Set<Class<?>> preInstanticateBeans) {
-        this.preInstanticateBeans = preInstanticateBeans;
+    public BeanFactory(Set<Class<?>> preInstantiateBeans) {
+        this.preInstantiateBeans = preInstantiateBeans;
     }
 
     @SuppressWarnings("unchecked")
@@ -24,6 +30,42 @@ public class BeanFactory {
     }
 
     public void initialize() {
+        preInstantiateBeans.forEach(this::instantiate);
+    }
 
+    private Object instantiate(Class<?> clazz) {
+        if (beans.containsKey(clazz)) {
+            return beans.get(clazz);
+        }
+
+        Constructor<?> constructor = BeanFactoryUtils.getInjectedConstructor(clazz);
+        Object instance = createBeanInstance(clazz, constructor);
+        beans.put(clazz, instance);
+        return instance;
+    }
+
+    private Object createBeanInstance(Class<?> clazz, Constructor<?> constructor) {
+        if (constructor == null) {
+            return BeanUtils.instantiateClass(clazz);
+        }
+        return instantiateConstructor(constructor);
+    }
+
+    private Object instantiateConstructor(Constructor<?> constructor) {
+        Class<?>[] parameterTypes = constructor.getParameterTypes();
+        List<Object> arguments = Lists.newArrayList();
+        for (Class<?> parameterType : parameterTypes) {
+            Class<?> concreteClass = BeanFactoryUtils.findConcreteClass(parameterType, preInstantiateBeans);
+            arguments.add(instantiate(concreteClass));
+        }
+        return BeanUtils.instantiateClass(constructor, arguments.toArray());
+    }
+
+    public Map<Class<?>, Object> getControllers() {
+        Map<Class<?>, Object> controllers = Maps.newHashMap();
+        preInstantiateBeans.stream()
+            .filter(clazz -> clazz.isAnnotationPresent(Controller.class))
+            .forEach(clazz -> controllers.put(clazz, beans.get(clazz)));
+        return controllers;
     }
 }
