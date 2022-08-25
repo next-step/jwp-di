7555aad4bb85ff4e1a8ed98fc8fd0c248a967cfb
devyonghee <devyonghee@gmail.com>
2022-08-25 22:50:04 +0900

    feat(BeanFactory.java): 빈 생성 기능 추가

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -1,21 +1,30 @@
 package core.di.factory;
 
-import com.google.common.collect.Maps;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.util.Assert;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.Objects.requireNonNullElseGet;
 
 public class BeanFactory {
-    private static final Logger logger = LoggerFactory.getLogger(BeanFactory.class);
 
-    private Set<Class<?>> preInstanticateBeans;
+    private final BeanScanner scanner;
+    private final Map<Class<?>, Object> beans = new HashMap<>();
 
-    private Map<Class<?>, Object> beans = Maps.newHashMap();
+    private BeanFactory(BeanScanner scanner) {
+        Assert.notNull(scanner, "scanner must not be null");
+        this.scanner = scanner;
+    }
 
-    public BeanFactory(Set<Class<?>> preInstanticateBeans) {
-        this.preInstanticateBeans = preInstanticateBeans;
+    public static BeanFactory from(BeanScanner scanner) {
+        return new BeanFactory(scanner);
     }
 
     @SuppressWarnings("unchecked")
@@ -24,6 +33,45 @@ public class BeanFactory {
     }
 
     public void initialize() {
+        scanner.scan()
+                .forEach(this::bean);
+    }
 
+    public Map<Class<?>, Object> annotatedWith(Class<? extends Annotation> annotation) {
+        return beans.entrySet()
+                .stream()
+                .filter(entry -> entry.getKey().isAnnotationPresent(annotation))
+                .collect(Collectors.toUnmodifiableMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    private Object bean(Class<?> clazz) {
+        if (beans.containsKey(clazz)) {
+            return beans.get(clazz);
+        }
+        Object instance = instantiateClass(clazz);
+        beans.put(clazz, instance);
+        return instance;
+    }
+
+    private Object instantiateClass(Class<?> clazz) {
+        if (clazz.isInterface()) {
+            return bean(scanner.subTypeOf(clazz));
+        }
+        Constructor<?> constructor = requireNonNullElseGet(BeanFactoryUtils.getInjectedConstructor(clazz), () -> defaultConstructor(clazz));
+        return BeanUtils.instantiateClass(constructor, arguments(constructor));
+    }
+
+    private Object[] arguments(Constructor<?> constructor) {
+        return Stream.of(constructor.getParameterTypes())
+                .map(this::bean)
+                .toArray();
+    }
+
+    private Constructor<?> defaultConstructor(Class<?> clazz) {
+        try {
+            return clazz.getConstructor();
+        } catch (NoSuchMethodException e) {
+            throw new BeanCreationException(clazz.getName(), e);
+        }
     }
 }
--- src/test/java/core/di/factory/BeanFactoryTest.java
+++ src/test/java/core/di/factory/BeanFactoryTest.java
@@ -1,30 +1,19 @@
 package core.di.factory;
 
-import com.google.common.collect.Sets;
-import core.annotation.Repository;
-import core.annotation.Service;
-import core.annotation.web.Controller;
 import core.di.factory.example.MyQnaService;
 import core.di.factory.example.QnaController;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.reflections.Reflections;
-
-import java.lang.annotation.Annotation;
-import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class BeanFactoryTest {
-    private Reflections reflections;
+
     private BeanFactory beanFactory;
 
     @BeforeEach
-    @SuppressWarnings("unchecked")
     void setup() {
-        reflections = new Reflections("core.di.factory.example");
-        Set<Class<?>> preInstanticateClazz = getTypesAnnotatedWith(Controller.class, Service.class, Repository.class);
-        beanFactory = new BeanFactory(preInstanticateClazz);
+        beanFactory = BeanFactory.from(BeanScanner.packages("core.di.factory.example"));
         beanFactory.initialize();
     }
 
@@ -38,14 +27,5 @@ class BeanFactoryTest {
         MyQnaService qnaService = qnaController.getQnaService();
         assertNotNull(qnaService.getUserRepository());
         assertNotNull(qnaService.getQuestionRepository());
-    }
-
-    @SuppressWarnings("unchecked")
-    private Set<Class<?>> getTypesAnnotatedWith(Class<? extends Annotation>... annotations) {
-        Set<Class<?>> beans = Sets.newHashSet();
-        for (Class<? extends Annotation> annotation : annotations) {
-            beans.addAll(reflections.getTypesAnnotatedWith(annotation));
-        }
-        return beans;
     }
 }
