ae6048bb7c04c7b43b06254c6edcc8c19291e14f
devyonghee <devyonghee@gmail.com>
2022-08-30 02:02:40 +0900

    refactor: 컴파일 에러 수정

--- src/test/java/core/mvc/tobe/AnnotationHandlerMappingTest.java
+++ src/test/java/core/mvc/tobe/AnnotationHandlerMappingTest.java
@@ -1,7 +1,6 @@
 package core.mvc.tobe;
 
-import core.di.factory.BeanFactory;
-import core.di.factory.BeanScanner;
+import core.di.factory.ApplicationContext;
 import next.dao.UserDao;
 import next.model.User;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,9 +17,7 @@ class AnnotationHandlerMappingTest {
 
     @BeforeEach
     void setup() {
-        BeanFactory beanFactory = BeanFactory.from(BeanScanner.packages("core.mvc.tobe"));
-        beanFactory.initialize();
-        handlerMapping = new AnnotationHandlerMapping(beanFactory);
+        handlerMapping = new AnnotationHandlerMapping(ApplicationContext.from("core.mvc.tobe"));
         handlerMapping.initialize();
 
         DBInitializer.initialize();
