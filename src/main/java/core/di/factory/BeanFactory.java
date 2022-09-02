2119c903935e0a5477d8fe50997a637e4c12eafc
HyungjuJang <dacapolife87@gmail.com>
2022-09-02 10:36:15 +0900

    feat : BeanFactory구현

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -4,6 +4,8 @@ import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
 import java.util.Set;
 
@@ -24,6 +26,42 @@ public class BeanFactory {
     }
 
     public void initialize() {
+        try {
+            for (Class<?> preInstanticateBean : preInstanticateBeans) {
+                beans.put(preInstanticateBean, instantiateClass(preInstanticateBean));
+            }
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
 
+    private Object instantiateClass(Class<?> clazz) throws InstantiationException, IllegalAccessException, InvocationTargetException {
+        logger.info("InstantiateClass : {}", clazz);
+        if (beans.containsKey(clazz)) {
+            logger.info("AlreadyInstantiateClass : {}", clazz);
+            return beans.get(clazz);
+        }
+
+        Class<?> concreteClass = BeanFactoryUtils.findConcreteClass(clazz, preInstanticateBeans);
+        Constructor<?> injectedConstructor = BeanFactoryUtils.getInjectedConstructor(concreteClass);
+        if (injectedConstructor == null) {
+            logger.info("NotExist InjectConstructor, Use Default Constructor : {}", concreteClass);
+            return concreteClass.newInstance();
+        }
+        return instantiateConstructor(injectedConstructor);
+    }
+
+    private Object instantiateConstructor(Constructor<?> constructor) throws InstantiationException, IllegalAccessException, InvocationTargetException {
+        Class<?>[] parameterTypes = constructor.getParameterTypes();
+        Object[] parameters = new Object[parameterTypes.length];
+        for (int i = 0; i < parameterTypes.length; i++) {
+            parameters[i] = instantiateClass(parameterTypes[i]);
+        }
+        logger.info("Create Instance Used Constructor");
+        return constructor.newInstance(parameters);
     }
 }
