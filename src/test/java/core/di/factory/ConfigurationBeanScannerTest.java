201d30845d32239c38716831e73396d60e7b48ab
황정현 <jhhwang@wemakeprice.com>
2022-09-06 20:42:55 +0900

    test configuration 테스트

--- src/test/java/core/di/factory/ConfigurationBeanScannerTest.java
+++ src/test/java/core/di/factory/ConfigurationBeanScannerTest.java
@@ -1,2 +1,24 @@
-package core.di.factory;public class ConfigurationBeanScannerTest {
+package core.di.factory;
+
+import core.di.ConfigurationBeanScanner;
+import core.di.factory.example.ExampleConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.sql.DataSource;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class ConfigurationBeanScannerTest {
+
+    @Test
+    void resister() throws InvocationTargetException, IllegalAccessException {
+        BeanFactory beanFactory = new BeanFactory();
+        ConfigurationBeanScanner cbs = new ConfigurationBeanScanner(beanFactory);
+        cbs.register(ExampleConfig.class);
+        beanFactory.initialize();
+
+        Assertions.assertThat(beanFactory.getBean(DataSource.class)).isNotNull();
+    }
+
 }
