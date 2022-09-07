61dab796c69b8f3c9361b701f0100dee27f8c20a
seondongpyo <seondongpyo@gmail.com>
2022-09-07 15:16:15 +0900

    refactor: 테스트용 설정 클래스 추가 및 적용

--- src/test/java/core/mvc/tobe/AnnotationHandlerMappingTest.java
+++ src/test/java/core/mvc/tobe/AnnotationHandlerMappingTest.java
@@ -1,15 +1,20 @@
 package core.mvc.tobe;
 
+import core.annotation.Bean;
+import core.annotation.ComponentScan;
+import core.annotation.Configuration;
 import core.di.AnnotationConfigApplicationContext;
 import core.di.ApplicationContext;
-import next.config.MyConfiguration;
 import next.dao.UserDao;
 import next.model.User;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import support.test.DBInitializer;
+
+import javax.sql.DataSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,7 +24,7 @@ class AnnotationHandlerMappingTest {
 
     @BeforeEach
     public void setup() {
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(MyConfiguration.class);
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(TestConfiguration.class);
         handlerMapping = new AnnotationHandlerMapping(applicationContext);
         handlerMapping.initialize();
         userDao = applicationContext.getBean(UserDao.class);
@@ -33,7 +38,7 @@ class AnnotationHandlerMappingTest {
         createUser(user);
         assertThat(userDao.findByUserId(user.getUserId())).isEqualTo(user);
 
-        MockHttpServletRequest request = new MockHttpServletRequest("GET", "/test/users");
+        MockHttpServletRequest request = new MockHttpServletRequest("GET", "/users");
         request.setParameter("userId", user.getUserId());
         MockHttpServletResponse response = new MockHttpServletResponse();
         HandlerExecution execution = (HandlerExecution)handlerMapping.getHandler(request);
@@ -43,7 +48,7 @@ class AnnotationHandlerMappingTest {
     }
 
     private void createUser(User user) throws Exception {
-        MockHttpServletRequest request = new MockHttpServletRequest("POST", "/test/users");
+        MockHttpServletRequest request = new MockHttpServletRequest("POST", "/users");
         request.setParameter("userId", user.getUserId());
         request.setParameter("password", user.getPassword());
         request.setParameter("name", user.getName());
@@ -51,5 +56,19 @@ class AnnotationHandlerMappingTest {
         MockHttpServletResponse response = new MockHttpServletResponse();
         HandlerExecution execution = (HandlerExecution)handlerMapping.getHandler(request);
         execution.handle(request, response);
+    }
+
+    @Configuration
+    @ComponentScan({"core.jdbc", "core.mvc.tobe", "next.dao"})
+    public static class TestConfiguration {
+        @Bean
+        public DataSource dataSource() {
+            BasicDataSource ds = new BasicDataSource();
+            ds.setDriverClassName("org.h2.Driver");
+            ds.setUrl("jdbc:h2:mem://localhost/~/jwp-di;MODE=MySQL;DB_CLOSE_DELAY=-1");
+            ds.setUsername("sa");
+            ds.setPassword("");
+            return ds;
+        }
     }
 }
