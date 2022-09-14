28295e417543bee99b6e07a56be2bd8df21687d5
황정현 <jhhwang@wemakeprice.com>
2022-09-14 17:11:56 +0900

    refactor applicationContext 역할 추가

--- src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
+++ src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,31 +1,71 @@
 package core.mvc.tobe;
 
 import com.google.common.collect.Maps;
+import core.annotation.web.RequestMapping;
 import core.annotation.web.RequestMethod;
-import core.di.BeanScanner;
+import core.di.factory.ApplicationContext;
+import core.di.factory.MyConfiguration;
 import core.mvc.HandlerMapping;
+import core.mvc.tobe.support.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
+import org.springframework.core.ParameterNameDiscoverer;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+
+import static java.util.Arrays.asList;
 
 public class AnnotationHandlerMapping implements HandlerMapping {
     private static final Logger logger = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
     private Object[] basePackage;
-    private BeanScanner beanScanner;
+    private final ApplicationContext applicationContext;
+    private static final ParameterNameDiscoverer nameDiscoverer = new LocalVariableTableParameterNameDiscoverer();
+
+
+    private static final List<ArgumentResolver> argumentResolvers = asList(
+            new HttpRequestArgumentResolver(),
+            new HttpResponseArgumentResolver(),
+            new RequestParamArgumentResolver(),
+            new PathVariableArgumentResolver(),
+            new ModelArgumentResolver()
+    );
+
 
     private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
 
     public AnnotationHandlerMapping(Object... basePackage) {
         this.basePackage = basePackage;
-        beanScanner = new BeanScanner();
+        applicationContext = new ApplicationContext(MyConfiguration.class);
     }
 
     public void initialize() {
         logger.info("## Initialized Annotation Handler Mapping");
-        handlerExecutions.putAll(beanScanner.scan(basePackage));
+        handlerExecutions.putAll(matchingController());
+    }
+
+    private Map<? extends HandlerKey, ? extends HandlerExecution> matchingController() {
+        Map<HandlerKey, HandlerExecution> handlerMap = Maps.newHashMap();
+        Map<Class<?>, Object> controllers = applicationContext.getControllers();
+        for (Class<?> controller : controllers.keySet()) {
+            makeHandlerExecutions(handlerMap, controllers.get(controller), controller.getMethods());
+        }
+        return handlerMap;
+    }
+
+    private void makeHandlerExecutions(Map<HandlerKey, HandlerExecution> handlerMap, Object target, Method[] methods) {
+        Arrays.stream(methods)
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .forEach(m -> {
+                    HandlerKey handlerKey = new HandlerKey(m.getAnnotation(RequestMapping.class).value(), m.getAnnotation(RequestMapping.class).method());
+                    HandlerExecution handlerExecution = new HandlerExecution(nameDiscoverer, argumentResolvers, target, m);
+                    handlerMap.put(handlerKey, handlerExecution);
+                });
     }
 
     public Object getHandler(HttpServletRequest request) {
