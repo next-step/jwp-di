c75c5ab9281597ee35963d655cefd68c024f3b10
hwangjeonghyeon1 <deert202@naver.com>
2022-09-19 18:52:23 +0900

    refactor 빈 등록시점 변경

--- src/main/java/core/di/factory/ApplicationContext.java
+++ src/main/java/core/di/factory/ApplicationContext.java
@@ -4,25 +4,27 @@ import core.annotation.ComponentScan;
 
 import java.util.Arrays;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class ApplicationContext {
     private final BeanFactory beanFactory;
 
     public ApplicationContext(Class<?>... configurations) {
-        beanFactory = new BeanFactory();
-        ConfigurationBeanScanner cbs = new ConfigurationBeanScanner(beanFactory);
+        BeanDefinitions beanDefinitions = new BeanDefinitions();
+        beanFactory = new BeanFactory(beanDefinitions);
+        ConfigurationBeanScanner cbs = new ConfigurationBeanScanner(beanDefinitions);
         cbs.register(configurations);
-        ClassPathBeanScanner cdbs = new ClassPathBeanScanner(beanFactory);
+        ClassPathBeanScanner cdbs = new ClassPathBeanScanner(beanDefinitions);
         cdbs.doScan(getBasePackages(configurations));
-        beanFactory.initialize();
+        beanFactory.register();
     }
 
     private Object[] getBasePackages(Class<?>[] configurations) {
         return Arrays.stream(configurations)
                 .filter(configuration -> configuration.isAnnotationPresent(ComponentScan.class))
-                .map(configuration -> configuration.getAnnotation(ComponentScan.class))
-                .map(ComponentScan::basePackages)
-                .flatMap(Arrays::stream).toArray();
+                .map(clazz -> clazz.getAnnotation(ComponentScan.class).value())
+                .flatMap(Stream::of)
+                .toArray();
     }
 
     public Map<Class<?>, Object> getControllers() {
