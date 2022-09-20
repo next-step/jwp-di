7a777c90f61a4981cdafabcab059aa3bcdd54b78
hwangjeonghyeon1 <deert202@naver.com>
2022-09-20 18:40:39 +0900

    refactor beanscanner interface 화

--- src/main/java/core/di/factory/scanner/ClassPathBeanScanner.java
+++ src/main/java/core/di/factory/scanner/ClassPathBeanScanner.java
@@ -1,21 +1,53 @@
-package core.di.factory;
+package core.di.factory.scanner;
 
+import com.google.common.collect.Sets;
+import core.annotation.ComponentScan;
+import core.annotation.Repository;
+import core.annotation.Service;
+import core.annotation.web.Controller;
+import core.di.factory.definition.BeanDefinition;
+import core.di.factory.definition.BeanDefinitionRegistry;
+import core.di.factory.definition.ClassPathBeanDefinition;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ClassPathBeanScanner {
+import java.lang.annotation.Annotation;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Stream;
 
-    private static final Logger logger = LoggerFactory.getLogger(ClassPathBeanScanner.class);
-
-    private BeanDefinitionRegistry beanDefinitions;
-
+public class ClassPathBeanScanner implements BeanScanner {
+    private Reflections reflections;
+    private BeanDefinitionRegistry beanDefinitionRegistry;
 
     public ClassPathBeanScanner(BeanDefinitionRegistry beanDefinitions) {
-        this.beanDefinitions = beanDefinitions;
+        this.beanDefinitionRegistry = beanDefinitions;
     }
 
-    public void doScan(Object... basePackage) {
-        BeanScanner scanner = new BeanScanner();
-        beanDefinitions.addAllPreInstantiateBeans(scanner.scan(basePackage));
+    @Override
+    public void doScan(Class<?>... configurations) {
+        beanDefinitionRegistry.addAllPreInstantiateBeans(scan(getBasePackages(configurations)));
+    }
+
+    private Set<Class<?>> scan(Object... basePackage) {
+        reflections = new Reflections(basePackage);
+        return getTypesAnnotatedWith(Controller.class, Service.class, Repository.class);
+    }
+
+    private Set<Class<?>> getTypesAnnotatedWith(Class<? extends Annotation>... annotations) {
+        Set<Class<?>> beans = Sets.newHashSet();
+        for (Class<? extends Annotation> annotation : annotations) {
+            beans.addAll(reflections.getTypesAnnotatedWith(annotation));
+        }
+        return beans;
+    }
+
+    private Object[] getBasePackages(Class<?>[] configurations) {
+        return Arrays.stream(configurations)
+                .filter(configuration -> configuration.isAnnotationPresent(ComponentScan.class))
+                .map(clazz -> clazz.getAnnotation(ComponentScan.class).value())
+                .flatMap(Stream::of)
+                .toArray();
     }
 }
--- src/main/java/core/di/factory/scanner/ConfigurationBeanScanner.java
+++ src/main/java/core/di/factory/scanner/ConfigurationBeanScanner.java
@@ -1,21 +1,25 @@
-package core.di.factory;
+package core.di.factory.scanner;
 
+import core.di.factory.BeanFactory;
+import core.di.factory.definition.BeanDefinition;
+import core.di.factory.definition.BeanDefinitionRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-public class ConfigurationBeanScanner {
-    private static final Logger logger = LoggerFactory.getLogger(BeanFactory.class);
+public class ConfigurationBeanScanner implements BeanScanner {
 
-    private final BeanDefinitionRegistry beanDefinitions;
+    private final BeanDefinitionRegistry beanDefinitionRegistry;
 
-    public ConfigurationBeanScanner(BeanDefinitionRegistry beanDefinitions) {
-        this.beanDefinitions = beanDefinitions;
+    public ConfigurationBeanScanner(BeanDefinitionRegistry beanDefinitionRegistry) {
+        this.beanDefinitionRegistry = beanDefinitionRegistry;
     }
+    @Override
+    public void doScan(Class<?>... configurations) {
 
-    public void register(Class<?>... configurations) {
-        beanDefinitions.addConfigurations(Arrays.stream(configurations)
+        beanDefinitionRegistry.addConfigurations(Arrays.stream(configurations)
                 .collect(Collectors.toList()));
     }
+
 }
