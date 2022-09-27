b70844be7e368b167a9e25e57d7483a1d3e63cb8
mincheolkk <kmc04080408@gmail.com>
2022-09-28 02:24:10 +0900

    feat : BeanScanner & BeanFactory 구현

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -1,11 +1,19 @@
 package core.di.factory;
 
 import com.google.common.collect.Maps;
+import core.annotation.web.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import static core.di.factory.BeanFactoryUtils.*;
 
 public class BeanFactory {
     private static final Logger logger = LoggerFactory.getLogger(BeanFactory.class);
@@ -24,6 +32,43 @@ public class BeanFactory {
     }
 
     public void initialize() {
+        for (Class<?> bean : preInstanticateBeans) {
+            beans.computeIfAbsent(bean, this::getInstance);
+        }
+    }
 
+    private Object getInstance(Class<?> clazz)  {
+        Constructor<?> constructor = getConstructor(clazz);
+        Class<?>[] parameterTypes = constructor.getParameterTypes();
+
+        Object[] objects = Arrays.stream(parameterTypes)
+                .map(it -> getInstance(BeanFactoryUtils.findConcreteClass(it, preInstanticateBeans)))
+                .toArray();
+
+        try {
+            return constructor.newInstance(objects);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Constructor<?> getConstructor(Class<?> concreteClass) {
+        Constructor<?> injectedConstructor = getInjectedConstructor(concreteClass);
+
+        if (Objects.nonNull(injectedConstructor)) {
+            return injectedConstructor;
+        }
+
+        try {
+            return concreteClass.getConstructor();
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Set<Class<?>> getControllerClass() {
+        return beans.keySet().stream()
+                .filter(clazz -> clazz.isAnnotationPresent(Controller.class))
+                .collect(Collectors.toUnmodifiableSet());
     }
 }
